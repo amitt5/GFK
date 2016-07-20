1210b65a4c3c55d2e3a484c7488876c3ca7113a8
Wouter Hulst <wouterhulst@wouters-mbp-2.home>
2016-07-20 03:22:04 +0200

    task 1 completed

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -8,57 +8,30 @@
       this.$http = $http;
       this.socket = socket;
       this.awesomeThings = [];
-      $scope.stringVal  = "";   
+      $scope.stringVal  = ""; 
+      $scope.stringVal1  = [];   
+      $scope.task1Visible = false;
+      $scope.task2Visible = false;
+      $scope.task3Visible = false;
+      $scope.task4Visible = false;
 
 
     $scope.task2 = function() {
-      console.log("task2");
+      $scope.task1Visible = false;
+      $scope.task2Visible = true;
+      $scope.task3Visible = false;
+      $scope.task4Visible = false;
       
-      $scope.chartTypes = [
-        {"id": "line", "title": "Line"},
-        {"id": "spline", "title": "Smooth line"},
-        {"id": "area", "title": "Area"},
-        {"id": "areaspline", "title": "Smooth area"},
-        {"id": "column", "title": "Column"},
-        {"id": "bar", "title": "Bar"},
-        {"id": "pie", "title": "Pie"},
-        {"id": "scatter", "title": "Scatter"}
-      ];
-
-      $scope.dashStyles = [
-        {"id": "Solid", "title": "Solid"},
-        {"id": "ShortDash", "title": "ShortDash"},
-        {"id": "ShortDot", "title": "ShortDot"},
-        {"id": "ShortDashDot", "title": "ShortDashDot"},
-        {"id": "ShortDashDotDot", "title": "ShortDashDotDot"},
-        {"id": "Dot", "title": "Dot"},
-        {"id": "Dash", "title": "Dash"},
-        {"id": "LongDash", "title": "LongDash"},
-        {"id": "DashDot", "title": "DashDot"},
-        {"id": "LongDashDot", "title": "LongDashDot"},
-        {"id": "LongDashDotDot", "title": "LongDashDotDot"}
-      ];
-
+     
       $scope.chartSeries = [
-        // {"name": "Some data 1", "data": [1, 2, 4, 7, 3]},
-        // {"name": "Some data 2", "data": [3, 1, null, 5, 2], connectNulls: true},
         {"name": "Answer-Yes", "data": response},
-        // {"name": "My Super Column", "data": [1, 3, 5, 7, 9]} node csv
-        // {"name": "Some data 3", "data": [9, 7, 5, 3, 1], type: "column"},
-        // {"name": "My Super Column", "data": [1, 3, 5, 7, 9], type: "column"}
-      ];
-
-      $scope.chartStack = [
-        {"id": '', "title": "No"},
-        {"id": "normal", "title": "Normal"},
-        {"id": "percent", "title": "Percent"}
       ];
 
       
       $scope.chartConfig = {
         options: {
           chart: {
-            type: 'line'//check this 
+            type: 'line'
           },
           plotOptions: {
             series: {
@@ -66,10 +39,9 @@
             }
           }
         },
-        series: $scope.chartSeries,//check this 
+        series: $scope.chartSeries,
         xAxis: {
-            categories: dates
-            // categories: ['Date 1', 'Date 2', 'Date 3', 'Date 4', 'Date 5', 'Date 6', 'Date 7', 'Date 8', 'Date 9', 'Date 10']
+            categories: uniqueDates
           },
         yAxis: {
             title: {
@@ -77,7 +49,7 @@
             }
           },  
         title: {
-          text: 'Hello world'
+          text: 'GFK Chart'
         },
         credits: {
           enabled: true
@@ -86,48 +58,68 @@
         size: {}
       }
 
-      // $scope.reflow = function () {
-      //   console.log("reflow");
-      //   $scope.$broadcast('highchartsng.reflow');
-      // };   
 
 
     }////end of function Task 2
 
-    $scope.task3 = function() {
+    $scope.task1 = function() {
+      $scope.task1Visible = true;
+      $scope.task2Visible = false;
+      $scope.task3Visible = false;
+      $scope.task4Visible = false;
+      var strNum = " ";
+      var isDirty = false;
+      // var stringVal1 = []
+      for (var i = 1; i<=100; i++){
+        strNum = "";
+        isDirty = false;
+        if (i % 3 === 0){
+          strNum += "Bizz";
+          isDirty = true;
+        }
+        if (i % 5 === 0){
+          strNum += "Appz";
+          isDirty = true;
+        }
+        if (isDirty === false){
+          strNum += i;
+        }
+        strNum += " ";
+        $scope.stringVal1[i]  = strNum;
+      }
+      console.log('stringVal1');
+      console.log($scope.stringVal1);
 
+    }////end of task 1
+
+
+    // $scope.task1 = function() {
+    //   $scope.task1Visible = true;
+    //   $scope.task2Visible = false;
+    //   $scope.task3Visible = false;
+    //   $scope.task4Visible = false;
       
-
-
-    }////end of task 3
-
-    //   $scope.$on('$destroy', function() {
-    //     socket.unsyncUpdates('thing');
-    //   });
-
-    //     $scope.task1 = function() {
-          
-    //     var strNum = " ";
-    //     var isDirty = false;
-    //     for (var i = 1; i<=100; i++){
-    //       isDirty = false;
-    //       if (i % 3 === 0){
-    //         strNum += "Bizz";
-    //         isDirty = true;
-    //       }
-    //       if (i % 5 === 0){
-    //         strNum += "Appz";
-    //         isDirty = true;
-    //       }
-    //       if (isDirty === false){
-    //         strNum += i;
-    //       }
-    //       strNum += " ";
+    //   var strNum = " ";
+    //   var isDirty = false;
+    //   for (var i = 1; i<=100; i++){
+    //     isDirty = false;
+    //     if (i % 3 === 0){
+    //       strNum += "Bizz";
+    //       isDirty = true;
     //     }
-    //     console.log(strNum);
-    //     $scope.stringVal  = strNum;
-    //   }  
-    
+    //     if (i % 5 === 0){
+    //       strNum += "Appz";
+    //       isDirty = true;
+    //     }
+    //     if (isDirty === false){
+    //       strNum += i;
+    //     }
+    //     strNum += " ";
+    //   }
+    //   console.log(strNum);
+    //   $scope.stringVal  = strNum;
+    // }////end of task 1
+
 
     // // $scope.task2 = function() {
           
@@ -162,17 +154,14 @@
 
   }// end of   class MainController {
 
-
-  var dates_yes = [];
-
-  var dates_no= [];
-  var dates= [];
+  var uniqueDates= [];
   var dates1= [];
   var ids= [];
   var response= [1,2,3,4,5,6,7,8,9,10,11,12];
 
   var numberOfRows;
   var numberOfCoulumns;
+  var numberOfDates;
 
    
   angular.module('gfkApp')
@@ -181,7 +170,6 @@
       controller: MainController
     })
     .directive('fileReader', function() {
-      console.log('hi then')
       return {
         scope: {
           fileReader:"="
@@ -209,6 +197,7 @@
                        // ids[i] = columns[1];
                        // response[i] = columns[2];
 
+                       //creating 2D array
                        numberOfCoulumns = columns.length;
                         MultiArray [i] = new Array(numberOfCoulumns);
                         for (var j = 0; j < numberOfCoulumns; j++) {
@@ -217,47 +206,75 @@
                         }
 
 
-                       // if(row[2] == 'yes'){
-                       //   console.log(row[2]);
-                       //   if(dates_yes[row[0]] === undefined){
-                       //      dates_yes[row[0]] = 1;
-                       //   }
-                       //   dates_yes[row[0]]++ ;
-                       // }else{
-                       //   if(dates_no[row[0]] === undefined){
-                       //      dates_no[row[0]] = 1;
-                       //   }                        
-                       //   dates_no[row[0]]++;
-                       // }
-
                     }
                     // create array with all the dates
-                    var x = 0;
-                    for(var k = 0; k < numberOfRows; k++){
-                        dates1[k] = MultiArray [k][x]
-                        // console.log(MultiArray [k][x]);
-                    }
-                   console.log("uniqudsdsdsde dates");
-                    console.log(dates.indexOf(dates1[4]));
-                    // console.log(dates1.indexOf(dates1[4]));
-                    
+                    // var x = 0;
+                    // for(var k = 0; k < numberOfRows; k++){
+                    //     dates1[k] = MultiArray [k][x];
+                    //     console.log("dates1");
+                    //     console.log(dates1);
+                    //     // console.log(MultiArray [k][x]);
+                    // }
 
-                  
+                    //creating array with unique dates
                     for(var z = 1; z < numberOfRows; z++){
-                      if (dates.indexOf(dates1[z]) == -1) {
-                        dates.push(dates1[z]);
+                      if (uniqueDates.indexOf(MultiArray[z][0]) == -1) {
+                        uniqueDates.push(MultiArray[z][0]);
                       }   
-                            // console.log(MultiArray [k][x]);
                     } 
+                    numberOfDates = uniqueDates.length;
 
-                   console.log("unique dates");
-                   // console.log(dates);
+                    var yesCounter=[];
+                    var totalRecordsInDateCounter=[]
+                    var percentageArray=[]
+                    for(var m = 0; m<numberOfDates; m++){
+                      yesCounter[m] = 0;
+                      totalRecordsInDateCounter[m] = 0;
+                      for(var z = 1; z < numberOfRows; z++){
+                        
+                        // console.log("array dates");
+                        // console.log(MultiArray[z][0]);
+                        // console.log("unique dates");
+                        // console.log(uniqueDates[m]);
+                        // console.log(MultiArray[z][0] == uniqueDates[m]);
+                        if (MultiArray[z][0] == uniqueDates[m]){
+                          // console.log("inside first if");
+                          // console.log(z);
+                          // console.log(m);
+                          // console.log(MultiArray[z][2]);
 
-                    // console.log("yes");
-                    // console.log(dates_yes);
-                    // console.log("no");
+                          if (MultiArray[z][2] == MultiArray[1][2]){
+                          // console.log("inside second if");
+                          // console.log(z);
+                          // console.log(m);                            
+                          yesCounter[m] += 1;
+                          // console.log("yescounter" + yesCounter[m]);
 
-                    console.log(dates);
+                          }
+                        totalRecordsInDateCounter[m]++;
+  
+                        }                
+
+                      }
+                        percentageArray[m] = yesCounter[m]*100/totalRecordsInDateCounter[m];
+
+                    }
+                    // console.log("yesCounter");
+                    // console.log(yesCounter);
+                    // console.log(percentageArray);
+
+
+
+
+                    // for(var z = 1; z < numberOfRows; z++){
+                    //   if (dates.indexOf(dates1[z]) == -1) {
+                    //     dates.push(dates1[z]);
+                    //   }   
+                    //         // console.log(MultiArray [k][x]);
+                    // } 
+
+                    // console.log("unique dates");
+                    // console.log(uniqueDates);
 
                     // console.log(scope.fileReader);
                   });
@@ -271,105 +288,3 @@
 })();
 
 
-
-// DATE;ID;ANSWER
-//  01/01/13;1;yes
-//  01/01/13;2;no
-//  01/01/13;3;no
-//  01/01/13;4;no
-//  01/01/13;5;yes
-//  01/01/13;6;yes
-//  01/01/13;7;yes
-//  01/01/13;8;yes
-//  01/01/13;9;yes
-//  01/01/13;10;yes
-//  02/01/13;11;yes
-//  02/01/13;12;yes
-//  02/01/13;13;yes
-//  02/01/13;14;no
-//  02/01/13;15;no
-//  02/01/13;16;no
-//  02/01/13;17;no
-//  02/01/13;18;yes
-//  02/01/13;19;yes
-//  02/01/13;20;yes
-//  03/01/13;21;yes
-//  03/01/13;22;yes
-//  03/01/13;23;no
-//  03/01/13;24;no
-//  03/01/13;25;no
-//  03/01/13;26;no
-//  03/01/13;27;yes
-//  03/01/13;28;yes
-//  03/01/13;29;yes
-//  03/01/13;30;yes
-//  04/01/13;31;yes
-//  04/01/13;32;no
-//  04/01/13;33;yes
-//  04/01/13;34;no
-//  04/01/13;35;yes
-//  04/01/13;36;no
-//  04/01/13;37;yes
-//  04/01/13;38;no
-//  04/01/13;39;yes
-//  04/01/13;40;no
-//  04/01/13;41;yes
-//  04/01/13;42;yes
-//  04/01/13;43;no
-//  04/01/13;44;yes
-//  05/01/13;45;no
-//  05/01/13;46;no
-//  05/01/13;47;no
-//  05/01/13;48;no
-//  05/01/13;49;no
-//  05/01/13;50;yes
-//  05/01/13;51;yes
-//  05/01/13;52;yes
-//  06/01/13;53;yes
-//  06/01/13;54;no
-//  06/01/13;55;no
-//  06/01/13;56;yes
-//  06/01/13;57;yes
-//  06/01/13;58;yes
-//  06/01/13;59;yes
-//  07/01/13;60;no
-//  07/01/13;61;yes
-//  07/01/13;62;yes
-//  07/01/13;63;yes
-//  07/01/13;64;yes
-//  08/01/13;65;no
-//  08/01/13;66;no
-//  08/01/13;67;yes
-//  08/01/13;68;yes
-//  08/01/13;69;
-//  08/01/13;70;yes
-//  08/01/13;71;yes
-//  08/01/13;72;yes
-//  08/01/13;73;yes
-//  09/01/13;74;yes
-//  09/01/13;75;yes
-//  09/01/13;76;no
-//  09/01/13;77;yes
-//  09/01/13;78;yes
-//  09/01/13;79;yes
-//  09/01/13;80;no
-//  09/01/13;81;yes
-//  09/01/13;82;
-//  09/01/13;83;yes
-//  09/01/13;84;yes
-//  10/01/13;85;yes
-//  10/01/13;86;yes
-//  10/01/13;87;no
-//  10/01/13;88;no
-//  10/01/13;89;yes
-//  11/01/13;90;yes
-//  11/01/13;91;yes
-//  11/01/13;92;no
-//  11/01/13;93;no
-//  12/01/13;94;yes
-//  12/01/13;95;yes
-//  12/01/13;96;yes
-//  12/01/13;97;no
-//  12/01/13;98;no
-//  12/01/13;99;yes
-//  12/01/13;100;yes
